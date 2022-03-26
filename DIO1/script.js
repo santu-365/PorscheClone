517c21b136d3a503f9fee8e5458056c3fb54f66f
Mark <faletar.marko@gmail.com>
2022-03-26 22:36:24 +0100

    Projekt1.sem

---
+++ DIO1/script.js
@@ -0,0 +1,5 @@
+const gumb = document.getElementsByClassName("dropMeni")[0]
+const linkovi = document.getElementsByClassName("linkovi")[0]
+gumb.addEventListener('click', () => {
+    linkovi.classList.toggle('active')
+})
